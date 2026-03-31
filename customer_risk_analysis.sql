a08e6b3c958db5ebb8e55cbb74f4057458bdff2d
oroochristopher-stack <oroochristopher@gmail.com>
2026-03-31 18:24:05 +0300

    Update customer_risk_analysis.sql

--- customer_risk_analysis.sql
+++ customer_risk_analysis.sql
@@ -1,2 +1,4 @@
 -- Bank Customer Risk Segmentation Analysis
+-- Explicitly setting job location to 'US' to align with the residency of the risk_analysis dataset.
+-- if operating with tables from different regions, this innitial client should remain region agnostic i.e not directed to any location since it would operate as a global code location
 WITH customer_segments AS (
