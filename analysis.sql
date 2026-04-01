4b8dc443bc572f36d4137b377683a00f647e338f
oroochristopher-stack <oroochristopher@gmail.com>
2026-04-01 20:11:56 +0300

    Update analysis.sql

--- analysis.sql
+++ analysis.sql
@@ -3,3 +3,4 @@
 -- Architecture: Implemented a singleton-pattern Client initialization to ensure regional consistency across all analytical modules.
 -- Explicitly setting job location to 'US' to align with the residency of the risk_analysis dataset.
 -- if operating with tables from different regions, this innitial client should remain region agnostic i.e not directed to any location since it would operate as a global code location
+STEP 3: Risk Tier Segmentation & Capital Exposure Analysis.
