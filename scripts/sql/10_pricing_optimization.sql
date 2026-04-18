376a5c40ac26d6b8390b3e9a8b2b0f44ccdac812
oroochristopher-stack <oroochristopher@gmail.com>
2026-04-18 19:45:11 +0300

    Update 10_pricing_optimization.sql

--- scripts/sql/10_pricing_optimization.sql
+++ scripts/sql/10_pricing_optimization.sql
@@ -9,7 +9,7 @@ WITH Segment_Profitability AS (
         COUNT(*) AS loan_count,
         ROUND(AVG(int_rate_clean), 2) AS avg_rev_rate,
         ROUND(AVG(loan_status) * 0.70 * 100, 2) AS risk_cost_rate
-    FROM `<YOUR_PROJECT_ID>.risk_analysis.v_hardened_loans`
+    FROM `{PROJECT_ID}.risk_analysis.v_hardened_loans`
     GROUP BY 1, 2
 )
 SELECT 
@@ -19,7 +19,12 @@ SELECT
     avg_rev_rate AS current_rate,
     risk_cost_rate AS cost_of_risk,
     ROUND(avg_rev_rate - risk_cost_rate, 2) AS net_risk_margin,
-    ROUND(risk_cost_rate + 2.0, 2) AS recommended_target_rate
+    
+  
+    CASE 
+        WHEN (avg_rev_rate - risk_cost_rate) >= 2.0 THEN avg_rev_rate 
+        ELSE ROUND(risk_cost_rate + 2.0, 2)
+    END AS final_recommended_rate
+    
 FROM Segment_Profitability
-WHERE (avg_rev_rate - risk_cost_rate) < 2.0 
-ORDER BY net_risk_margin ASC;
+ORDER BY risk_tier, person_home_ownership
