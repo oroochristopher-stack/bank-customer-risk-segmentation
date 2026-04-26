5ede4deae31d26c753d68944b5f755c30997ab0f
oroochristopher-stack <oroochristopher@gmail.com>
2026-04-26 16:23:34 +0300

    Delete scripts  directory

--- scripts /python/ config.py
+++
@@ -1,6 +0,0 @@
-# --- GLOBAL CONFIGURATION ---
-PROJECT_ID = 'bank-risk-project'
-DATA_LOCATION = 'US' 
-
-# Operational Risk: Query scan limit (50MB) to prevent billing spikes
-MAX_BYTES = 50 * 1024 * 1024
